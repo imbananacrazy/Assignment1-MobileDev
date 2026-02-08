5e446121afba36cb429ea5b42492714480a8b4fb
Dmpoke66 <daniel.maliczewski@edu.sait.ca>
2026-02-08 10:59:31 -0700

    Update alert-button.tsx

--- components/alert-button.tsx
+++ components/alert-button.tsx
@@ -1,12 +1,20 @@
+// Reusable button component that triggers a simple alert
+// Used to demonstrate basic user interaction
+
 import { StyleSheet, Text, View, Button} from 'react-native'
 import React from 'react'
 
+// Stateless functional component for displaying an alert button
+
 const AlertButton = () => {
   return (
-    <Button title="Alert Button" onPress={() => alert("Alert Button Pressed")} />
+    <Button title="Alert Button" onPress={() => alert("Alert Button Pressed")}// Displays alert when button is pressed 
+      />
   )
 }
 
 export default AlertButton
+
+// Styles can be added here if button customization is needed later
 
 const styles = StyleSheet.create({})
